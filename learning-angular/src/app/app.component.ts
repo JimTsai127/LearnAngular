6cf818f5e91d4023f7f1abf463c6d7be480af325
Hung-Yu Tsai <w0748194@myscc.ca>
2021-09-14 14:19:14 -0400

    Week 2 example

--- learning-angular/src/app/app.component.ts
+++ learning-angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MarvelHero} from "helper-files/MarvelHero";
+import {ContentList} from "helper-files/ContentList";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learning-angular';
+  static contentCount = 0;
+
+  public ngOnInit(): void {
+
+    let name: string;
+    name = "Peter"
+    console.log("Name is", name);
+
+    let contentItem: MarvelHero = {
+      id: 0,
+      name: "Iron Man",
+      imageURL: "https://angular.io/assets/images/logos/angular/angular.png",
+      description: "We will miss him"
+    }
+    this.processHero(contentItem);
+
+    // ContentList.contentCount = 1;
+    let list: ContentList;
+    list = new ContentList(contentItem);
+    console.log("We changed the hero inside", list.items);
+  }
+
+  public processHero(hero: MarvelHero): void {
+    console.log(hero.description);
+  }
 }
+
+
+
+
