870f84bc9b8440a49d674a5e5755a26e7ddd83f4
Hung-Yu Tsai <w0748194@myscc.ca>
2021-10-09 17:33:02 -0400

    Directive for changing text formatting upon hover and de-hover

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { MatchesStringPipe } from './matches-string.pipe';
+import { HoverStyleDirective } from './hover-style.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
     ContentListComponent,
-    MatchesStringPipe
+    MatchesStringPipe,
+    HoverStyleDirective
   ],
   imports: [
     BrowserModule,
---
+++ learning-angular/src/app/hover-style.directive.ts
@@ -0,0 +1,36 @@
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
+
+@Directive({
+  selector: '[appHoverStyle]'
+})
+export class HoverStyleDirective {
+  @Input() hstyle?: string; // Style of formatting
+
+  // Element referenced
+  constructor(private ele: ElementRef) {
+  }
+
+  // Execute when hovering over element
+  @HostListener('mouseenter') onHover() {
+    this.formatText(this.hstyle || "", true);
+  }
+
+  // Execute when de-hovering
+  @HostListener('mouseleave') onDehover() {
+    this.formatText(this.hstyle || "",false);
+  }
+
+  // Formats text or de-formats text given the style (e.g. bold, underline) and on/off state
+  formatText(style: string, state: boolean): void {
+    switch(style) {
+      case "underline":
+        this.ele.nativeElement.style.textDecoration = state ? "underline" : "none";
+        break;
+      case "bold":
+        this.ele.nativeElement.style.fontWeight = state ? "bold" : "normal";
+        break;
+      default:
+        break;
+    }
+  }
+}
