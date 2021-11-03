c1daa80bb0b3effe4c64166ae894291460195375
Hung-Yu Tsai <w0748194@myscc.ca>
2021-11-03 12:36:45 -0400

    Added service and database

--- learning-angular/src/app/content-list/content-list.component.ts
+++ learning-angular/src/app/content-list/content-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { Content } from "../helper-files/content-interface";
+import {ContentService} from "../services/content.service";
 
 @Component({
   selector: 'app-content-list',
@@ -7,62 +8,17 @@ import { Content } from "../helper-files/content-interface";
   styleUrls: ['./content-list.component.css']
 })
 export class ContentListComponent implements OnInit {
-  contentArray: Content[]; // Holds an array of content items
+  contentArray: Content[] = []; // Holds an array of content items
   public item: any; // Stores the user's search item
 
-  constructor() {
+  constructor(private contentService: ContentService) {
     this.item = "";
-
-    // List of books
-    this.contentArray = [{
-      id: 0,
-      author: 'Cornelia Funke',
-      imgUrl: "https://upload.wikimedia.org/wikipedia/en/6/60/Thieflordbookcover.jpg",
-      type: "Paperback",
-      title: "The Thief Lord",
-      body: "A good book",
-      tags: ["fantasy", "young adult"]
-    }, {
-      id: 1,
-      author: "J.K. Rowling",
-      imgUrl: "https://upload.wikimedia.org/wikipedia/en/a/a9/Harry_Potter_and_the_Deathly_Hallows.jpg",
-      type: "Hardcover",
-      title: "Harry Potter and the Deathly Hallows",
-      body: "Final book in the series",
-      tags: ["fantasy"]
-    }, {
-      id: 2,
-      author: 'Lemony Snicket',
-      type: "Hardcover",
-      title: "The Series of Unfortunate Events 13",
-      body: "Last known book",
-      tags: ["psychological"]
-    }, {
-      id: 3,
-      author: 'Christopher Paolini',
-      imgUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/Eragon_book_cover.png/200px-Eragon_book_cover.png",
-      type: "Paperback",
-      title: "Eragon",
-      body: "First book in the series",
-      tags: ["fantasy"]
-    }, {
-      id: 4,
-      author: "Cornelia Funke",
-      imgUrl: "https://images-na.ssl-images-amazon.com/images/I/81ftas+eTwL.jpg",
-      type: "Hardcover",
-      title: "Inkspell",
-      body: "Good book",
-      tags: ["fantasy", "magic"]
-    }, {
-      id: 5,
-      author: "Unknown",
-      type: "Hardcover",
-      title: "Inkspell",
-      body: "Duplicate"
-    }];
   }
 
   ngOnInit(): void {
+    this.contentService.getContentObs().subscribe(
+      contentArray => this.contentArray = contentArray
+    );
   }
 
   // When user requests to find a book, highlight the book and show a message
---
+++ learning-angular/src/app/helper-files/contentDb.ts
@@ -0,0 +1,48 @@
+import {Content} from "./content-interface";
+
+export const CONTENT: Content[] = [{
+  id: 0,
+  author: 'Cornelia Funke',
+  imgUrl: "https://upload.wikimedia.org/wikipedia/en/6/60/Thieflordbookcover.jpg",
+  type: "Paperback",
+  title: "The Thief Lord",
+  body: "A good book",
+  tags: ["fantasy", "young adult"]
+}, {
+  id: 1,
+  author: "J.K. Rowling",
+  imgUrl: "https://upload.wikimedia.org/wikipedia/en/a/a9/Harry_Potter_and_the_Deathly_Hallows.jpg",
+  type: "Hardcover",
+  title: "Harry Potter and the Deathly Hallows",
+  body: "Final book in the series",
+  tags: ["fantasy"]
+}, {
+  id: 2,
+  author: 'Lemony Snicket',
+  type: "Hardcover",
+  title: "The Series of Unfortunate Events 13",
+  body: "Last known book",
+  tags: ["psychological"]
+}, {
+  id: 3,
+  author: 'Christopher Paolini',
+  imgUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/Eragon_book_cover.png/200px-Eragon_book_cover.png",
+  type: "Paperback",
+  title: "Eragon",
+  body: "First book in the series",
+  tags: ["fantasy"]
+}, {
+  id: 4,
+  author: "Cornelia Funke",
+  imgUrl: "https://images-na.ssl-images-amazon.com/images/I/81ftas+eTwL.jpg",
+  type: "Hardcover",
+  title: "Inkspell",
+  body: "Good book",
+  tags: ["fantasy", "magic"]
+}, {
+  id: 5,
+  author: "Unknown",
+  type: "Hardcover",
+  title: "Inkspell",
+  body: "Duplicate"
+}];
