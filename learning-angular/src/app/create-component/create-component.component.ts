a63b7a2228d3b1a7e1530b875153ac75e47d3d2f
Hung-Yu Tsai <w0748194@myscc.ca>
2021-11-20 06:58:41 -0500

    Got dialog to add book, everything working except tags

--- learning-angular/src/app/create-component/create-component.component.ts
+++ learning-angular/src/app/create-component/create-component.component.ts
@@ -13,7 +13,7 @@ export class CreateComponentComponent implements OnInit {
 
   @Output() newBookEvent = new EventEmitter<Content>();
   @Output() updateBookEvent = new EventEmitter<Content>();
-  
+
   newBook: Content;
   msg: any;
   tempTags = "";
@@ -34,24 +34,24 @@ export class CreateComponentComponent implements OnInit {
     const dialogRef = this.dialog.open(CreateDialog, {
       height: '620px',
       width: '600px',
-      data: {author: this.newBook.author, imgUrl: this.newBook.imgUrl, type: this.newBook.type, title: this.newBook.title, body: this.newBook.body, tags: this.newBook.tags}
+      data: {author: this.newBook.author, imgUrl: this.newBook.imgUrl, type: this.newBook.type, title: this.newBook.title, body: this.newBook.body, tags: this.tempTags}
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      // this.animal = result;
+      this.newBook = result;
       this.add();
     });
   }
 
   add(): void {
     // Check if required fields are empty
-    if(this.newBook.author && this.newBook.title && this.newBook.body) {
+    // if(this.newBook.author && this.newBook.title && this.newBook.body) {
       this.newBook.tags = this.tempTags.split(",");
       this.contentService.addContent(this.newBook).subscribe(newBookItem => {
         this.messageService.add("Added content has an id of: " + newBookItem.id);
         this.newBookEvent.emit(this.newBook);
       })
-    }
+    // }
   }
 
   update(): void {
@@ -76,18 +76,9 @@ export class CreateComponentComponent implements OnInit {
   templateUrl: './create-dialog.html'
 })
 export class CreateDialog {
-  newBook: Content;
-  tempTags = "";
-  tempId = "";
-
   constructor(
     public dialogRef: MatDialogRef<CreateDialog>,
               @Inject(MAT_DIALOG_DATA) public data: Content) {
-    this.newBook = {
-      author: '',
-      title: '',
-      body: ''
-    };
   }
 
   onNoClick(): void {
